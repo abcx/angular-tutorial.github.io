d80a5628efb68297206cd2b5ec85d75ed36ba6aa
Adam Bartniczak <adam.bartniczak@agora.pl>
2017-09-19 15:41:43 +0200

    Initial commit

---
+++ assets/.config.BAK.js
@@ -0,0 +1,4 @@
+var config = {
+    "server": "http://localhost:3000",
+    "assets": "http://webmaster.gazeta.pl/~icp/projekty/abcx/presentation/"
+};
---
+++ assets/.config.js
@@ -0,0 +1,8 @@
+var config = {
+    "server": "http://localhost:3000",
+    "assets": "./", // "http://webmaster.gazeta.pl/~icp/projekty/abcx/presentation/"
+    "saveJson": false,
+    "background": {
+        "class": "border_1"
+    }
+};
